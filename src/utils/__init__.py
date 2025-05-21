f866923e4eedfb535d93ac1f73efc8f2a9d9cd69
Gustaw Opielka <gucioopielka@gmail.com>
2025-05-21 15:17:43 +0300

    lumi start

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,20 @@
 import os
 os.environ['TOKENIZERS_PARALLELISM'] = 'false' # Suppressing the stupid warning
 
+if any(env in os.environ for env in ['SLURM_JOB_ID', 'SLURM_CLUSTER_NAME']):
+    # Monkey patch nnsight logger
+    import logging
+    import logging.handlers as _handlers
+
+    # Replace RotatingFileHandler with something that never touches disk
+    class _NoOpHandler(logging.Handler):
+        def __init__(self, *args, **kw):
+            super().__init__()
+        def emit(self, record):
+            pass
+
+    _handlers.RotatingFileHandler = _NoOpHandler
+
 import nnsight
 if not nnsight.CONFIG.API.APIKEY:
     if os.environ.get("NNSIGHT_API_KEY"):
